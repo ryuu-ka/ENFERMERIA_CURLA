210ce0375042b99da48f8777e40d3d62e5aa7494
Alejandra Orellana <alejandraorellana1015@gmail.com>
2024-04-16 13:47:16 -0600

    Deploy Initial without workflow

--- variables.js
+++ variables.js
@@ -1,12 +1,226 @@
 const handlerBarsContext = {
-    "/index.html": {
-        navh: [
+    "/pgs/faq.html": {
+        faq: [
             {
-                "hrf": "",
-                "page": ""
+                "pp": "¿Cuál es el plan de estudios de la carrera de Enfermería en CURLA?",
+                "rr": "El plan de estudios de Enfermería en CURLA incluye una combinación de cursos teóricos y prácticos que abarcan áreas como quimica, fisiología, farmacología, epidemiologia, entre otros."
             },
+            {
+                "pp": "¿Cuántos años dura la carrera de Enfermería en CURLA?",
+                "rr": "La carrera de Enfermería en CURLA tiene una duración de aproximadamente cinco años, divididos en tres periodos cada uno."
+            },
+            {
+                "pp": "¿Qué requisitos de admisión debo cumplir para ingresar a la Facultad de Enfermería en CURLA?",
+                "rr": "Los requisitos de admisión pueden variar, pero suelen incluir haber completado la educación secundaria, aprobar un examen de ingreso y cumplir con ciertos requisitos de salud."
+            },
+            {
+                "pp": "¿Cuáles son las oportunidades de prácticas profesionales que ofrece la Facultad de Enfermería en CURLA?",
+                "rr": "La facultad ofrece oportunidades de prácticas en hospitales locales, centros de salud comunitarios y otras instituciones de atención médica para que los estudiantes adquieran experiencia práctica."
+            },
+            {
+                "pp": "¿Qué tipo de actividades extracurriculares están disponibles para los estudiantes de Enfermería en CURLA?",
+                "rr": "Los estudiantes pueden participar en clubes estudiantiles, eventos de concienciación sobre la salud en la comunidad, y actividades de voluntariado en colaboración con organizaciones locales."
+            },
+            {
+                "pp": "¿Cuál es la carga académica típica por semestre para los estudiantes de Enfermería en CURLA?",
+                "rr": "La carga académica varía, pero los estudiantes suelen tomar una combinación de cursos teóricos y clínicos que suman aproximadamente 15 a 18 horas de clases por semana."
+            },
+            {
+                "pp": "¿Qué oportunidades de investigación ofrece la Facultad de Enfermería en CURLA?",
+                "rr": "La facultad puede ofrecer oportunidades de investigación a través de proyectos supervisados por profesores o colaboraciones con otras instituciones de investigación."
+            },
+            {
+                "pp": "¿Cómo se preparan los estudiantes de Enfermería en CURLA para los exámenes de certificación profesional?",
+                "rr": "Los estudiantes reciben orientación académica y tienen acceso a recursos de estudio para prepararse para los exámenes de certificación profesional, además de recibir práctica clínica supervisada."
+            },
+            {
+                "pp": "¿Cuáles son las opciones de especialización disponibles para los graduados de Enfermería en CURLA?",
+                "rr": "Las opciones de especialización pueden incluir enfermería pediátrica, enfermería geriátrica, enfermería de cuidados intensivos, entre otras, dependiendo de los intereses y necesidades del estudiante."
+            },
+            {
+                "pp": "¿Hay oportunidades de intercambio estudiantil para los estudiantes de Enfermería en CURLA?",
+                "rr": "La facultad puede tener programas de intercambio estudiantil con otras universidades o instituciones de salud en Honduras o en el extranjero."
+            },
+            {
+                "pp": "¿Cómo se mantiene actualizado el plan de estudios de Enfermería en CURLA para reflejar las últimas prácticas y avances en el campo de la salud?",
+                "rr": "El plan de estudios se actualiza periódicamente en consulta con profesionales de la salud y expertos en educación para garantizar que refleje las últimas prácticas y avances en el campo de la enfermería."
+            },
+            {
+                "pp": "¿Qué oportunidades de empleo existen para los graduados de Enfermería en CURLA?",
+                "rr": "Los graduados pueden encontrar empleo en hospitales, clínicas, centros de atención primaria, hogares de ancianos, instituciones educativas y otras organizaciones de salud tanto en Honduras como en el extranjero."
+            },
+            {
+                "pp": "¿La Facultad de Enfermería en CURLA ofrece programas de educación continua para profesionales en ejercicio?",
+                "rr": "Sí, la facultad puede ofrecer programas de educación continua y talleres para enfermeros en ejercicio que deseen actualizar sus conocimientos y habilidades."
+            },
+            {
+                "pp": "¿Cómo se apoya a los estudiantes durante su proceso de pasantías clínicas en la Facultad de Enfermería en CURLA?",
+                "rr": "Los estudiantes reciben supervisión y orientación por parte de profesores y profesionales de la salud durante sus pasantías clínicas para garantizar una experiencia de aprendizaje efectiva y segura."
+            },
+            {
+                "pp": "¿Cuál es la tasa de empleo y satisfacción laboral de los graduados de Enfermería de CURLA?",
+                "rr": "La facultad puede proporcionar datos sobre la tasa de empleo y la satisfacción laboral de los graduados, así como testimonios de exalumnos, para ofrecer una visión general de las perspectivas profesionales de la carrera de Enfermería en CURLA."
+            }
+        ], 
 
+        
+    }, 
+
+    "/pgs/perfilcarrera.html": {
+        af: [
+            {
+                "titulo": "Atención integral: ",
+                "descrip": "Cuidados directos en salud con equidad, eficiencia y eficacia al individuo, la familia y la comunidad, con enfoque participativo y epidemiológico."
+            },
+            {
+                "titulo": "Administración: ",
+                "descrip": "Planificar, coordinar, ejecutar, evaluar y liderar los diferentes procesos de trabajo,programas, proyectos y servicios de salud."
+            },
+            {
+                "titulo": "Educación: ",
+                "descrip": "Su proceso de monitoreo desarrolla programas de: Educación permanente de la actividad educativa a la población. Educación continua y permanente personal de enfermeria y otros. Formación de recursos humanos."
+            },
+            {
+                "titulo": "Investigación",
+                "descrip": "Utilizada como herramienta de trabajo que le permite identificar necesidades y problemas en el área de trabajo, orientada a la transformación de los servicios."
+            }
+        ], 
+        tt:[
+            {
+                "tarea": "Identificar necesidades y problemas en el área de salud tanto hospitalario como comunitario."
+            },
+            {
+                "tarea": "Elaborar proyectos orientados a brindar educacién en Salud en forrna continua y permanente."
+            },
+            {
+                "tarea": "Planificar, coordinar, ejecutar y evaluar los servicios de salud de la población."
+            },
+            {
+                "tarea": "Participar en cursos, seminarios, talleres y eventos científicos."
+            },
+            {
+                "tarea": "Realizar prácticas dirigidas, en instituciones afines al campo protesional."
+            },
+            {
+                "tarea": "Analizar la situación de Salud de los diferente servicios de atención en salud."
+            },
+            {
+                "tarea": "Investigar sobre el proceso de atención de los diferentes campos de practica."
+            },
+            {
+                "tarea": "Presentar propuestas encaminadas a mejorar los servicios de salud."
+            }
         ],
+        hc:[
+            {
+                "habil": "Capacidad para identificar la realidad nacional y la heterogeneidad de los diferentes grupos sociales. "
+            },
+            {
+                "habil": "Desarrollar un pensamiento critico y reflexivo en la explicación biológica e histórica cultural."
+            },
+            {
+                "habil": "Asumir un papel de solidaridad y compromiso consigo mismo y grupos humanos."
+            },
+            {
+                "habil": " Crear sus propios modelos y soluciones a las diferentes situaciones que afronta."
+            },
+            {
+                "habil": "Facilidad para manejar diversas gerenciales como herramientas de trabajo cotidiano."
+            },
+            {
+                "habil": "Capacidad para la negociación permanente y de una forma sostenible a nivel multisectorial."
+            },
+            {
+                "habil": "Facilidad para interrelacionarse con diferentes grupos sociales."
+            },
+            {
+                "habil": "Liderar procesos de promoción, prevención y transformación de los servicios de salud."
+            },
+            {
+                "habil": "Capacidad en el manejo del recurso humano  y tecnológico."
+            }
+        ],
+        al:[
+            {
+                "lugar": "Instituciones Publicas.",
+            },
+            {
+                "lugar": "Instituciones Privadas.",
+            },
+            {
+                "lugar": "Instituciones Semi-Autónomas.",
+            },
+            {
+                "lugar": "Organizaciones de Desarrollo No Gubernamentales.",
+            },
+            {
+                "lugar": "Organizaciones Internacionales.",
+            },
+            {
+                "lugar": "Ejercicio Liberal de la Profesiön. ",
+            },
+            {
+                "lugar": "Jefatura de Departamento, de Servicio, Unidad Hospitalaria y Comunitaria.",
+            },
+            {
+                "lugar": "Jefatura de Enfermería en Región, Área y Sector. ",
+            },
+            {
+                "lugar": "Coordinación de Programas Especiales.",
+            },
+            {
+                "lugar": "Coordinación y Docencia en Centros de Educación Media y Superior.",
+            },
+            {
+                "lugar": "Supervisión de Servicios, Programas y Proyectos.",
+            },
+            {
+                "lugar": "Coordinación de Programa de Salud en la Industria, Banca y el Comercio.",
+            },
+            {
+                "lugar": "Gerencia de Programas de Salud.",
+            },
+            {
+                "lugar": "Dirección de Centros Educativos de Enfermería.",
+            },
+            {
+                "lugar": "Docente en Centros Formadores de Recursos a nivel universitario y de Escuelas Auxiliares de Enfermería.",
+            },
+            {
+                "lugar": "Practica Privada, Consultoría y Otros.",
+            },
+            {
+                "lugar": "Jefatura de Programas.",
+            }
+        ],
+        ac:[
+            {
+                "titulo": "Duración de la Carrera: ",
+                "descrip": "5 años. 4 años de desarrollo asignaturas y 1 año de Servicio Social en diferentes zonas del país."
+            },
+            {
+                "descrip": "Los estudiantes realizan el servicio social con Salario - Beca."
+            },
+            {
+                "titulo": "Grado académico: ",
+                "descrip": "Licenciatura."
+            },
+            {
+                "titulo": " Titulo a obtener: ",
+                "descrip": "Licenciado(a) en Enfermería."
+            },
+            {
+                "descrip": "Es una carrera que exige dedicación exclusiva en tiempo y de preferencia no tener otras ocupaciones."
+            },
+            {
+                "titulo": "Requisitos de Graduación:",
+                "descrip": "Haber cumplido el plan de estudio y presentar monografía al finalizar el Servicio Social."
+            },
+            {
+                "titulo": " Posibilidades de Estudio a nivel de especialidades en: ",
+                "descrip": "Atención Integral del Niño y el Adolescente, Salud Materno Perinatal, Salud Familiar, Quirófanos."
+            }
+        ]
     }
 }
 
