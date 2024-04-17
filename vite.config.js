07123e850cc9f3d2b35dabd125184e44bcbe9a99
Alejandra Orellana <alejandraorellana1015@gmail.com>
2024-04-17 01:24:20 -0600

    Deploy Initial without workflow

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-    base: "/Enfermeria_CURLA/",
+    base: "/ENFERMERIA_CURLA/",
     appType: 'mpa',
     build: {
         rollupOptions: {
